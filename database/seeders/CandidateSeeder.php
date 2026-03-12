194c5fc5496c75397190f51600923cda52a885e3
carllqt <reycarlmedico@gmail.com>
2026-03-12 09:13:32 +0800

    oks na

--- database/seeders/CandidateSeeder.php
+++ database/seeders/CandidateSeeder.php
@@ -10,18 +10,28 @@ class CandidateSeeder extends Seeder
     public function run(): void
     {
         $femaleCandidates = [
-            ['Elsie', 'Valdez'],
-            ['Nicole Kim', 'Paguirigan'],
-            ['Andrea Leigh', 'Fernandez',],
-            ['Kenneth Clier', 'Bunagan',],
-            ['Ma. Kathleen Joyce', 'Cabanlong',]
+            ['Khayla', 'Fernandez'],
+            ['Lyka', 'Galam'],
+            ['Jazlyn', 'Seguro'],
+            ['Heart Angel', 'Visaya'],
+            ['Heart Angel', 'Pillos'],
+            ['Althea', 'Vinoya'],
+            ['Precious Joy', 'Acido'],
+            ['Mery Rose', 'Pintucan'],
+            ['Vanessa', 'Tuazon'],
+            ['Reighn', 'Carnate'],
+            ['Sharah Mayne', 'Mendieta'],
+            ['Thea May', 'Macalinao'],
+            ['Hanna Jade', 'Balber'],
+            ['Allysa', 'Cadiente'],
+            ['Mhay Ann', 'Dumalag']
         ];
 
         foreach ($femaleCandidates as $index => $candidate) {
             Candidate::create([
-                'first_name'      => $candidate[0],
-                'last_name'       => $candidate[1],
-                'profile_img'     => "candidates/female/" . ($index + 1) . ".jpg",
+                'first_name'       => $candidate[0],
+                'last_name'        => $candidate[1],
+                'profile_img'      => "candidates/" . ($index + 1) . ".jpg",
                 'candidate_number' => $index + 1,
             ]);
         }
